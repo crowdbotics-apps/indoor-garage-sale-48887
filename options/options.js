2461a585eaa683f7d212a07b50e323fec3e532ac
Crowdbotics <team@crowdbotics.com>
2024-07-28 07:13:05 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "indoor-garage-sale-48887",
+  url: "https://indoor-garage-sale-48887.botics.co",
+  api: "https://indoor-garage-sale-48887.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
